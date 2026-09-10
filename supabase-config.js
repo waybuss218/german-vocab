175c55cbab7a43dc84b391d45dc9404a9de52ff6
waybuss218 <2745717839@qq.com>
2026-09-10 22:23:18 +0800

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -3,5 +3,5 @@
 // This file is intentionally public; NEVER put an sb_secret_ key here.
 window.SUPABASE_CONFIG = {
   url: 'https://litsalynlkxvwenlapne.supabase.co',
-  publishableKey: 'PASTE_YOUR_SB_PUBLISHABLE_KEY_HERE'
+  publishableKey: 'sb_publishable_Ra2y-U7GbJYnLue8TYkuzw_JnbQYo5p'
 };
